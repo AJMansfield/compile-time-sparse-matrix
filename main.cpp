54ff20e9ff48253f651bec4d5530da0ade4e7257
Anson Mansfield <anson.mansfield@gmail.com>
2023-02-03 16:16:32 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
-#include <cstdint>
-#include <typeinfo>
-#include <boost/core/demangle.hpp>
 #include "sparse/csr_matrix.h"
 #include "dbg.h"
 
-using std::cout, std::size_t;
 using value_t = signed char;
 using subscript_t = unsigned char;
 using index_t = unsigned char;
@@ -24,15 +19,7 @@ constexpr std::array coo_data = std::to_array<entry_t>({
     {0,4,94},
     {0,0,99},
 });
-
-constexpr auto coo_mat = sparse::make_coo_matrix(coo_data, -1);
-constexpr auto csr_mat_1 = sparse::make_csr_matrix_from_coo(index_t, coo_mat);
-
-constexpr auto csr_mat_2 = sparse::make_csr_matrix(index_t, coo_data, -1);
-
-constexpr auto csr_mat_3 = sparse::make_csr_matrix(index_t, std::to_array<entry_t>({{0,255,1}}), -1);
-constexpr auto csr_mat_4 = sparse::make_csr_matrix(index_t, std::to_array<entry_t>({{254,0,1}}), -1);
-constexpr auto csr_mat_5 = sparse::make_csr_matrix(index_t, std::to_array<entry_t>({{254,255,1}}), -1);
+constexpr auto csr_mat = sparse::make_csr_matrix(coo_data, -1);
 
 int main() {
     dbg_v(sizeof(coo_data));
@@ -42,11 +29,7 @@ int main() {
     dbg_th(&coo_mat);
     dbg_th(&(coo_mat.data));
 
-    dbg_csr_mat(csr_mat_1);
-    dbg_csr_mat(csr_mat_2);
-    dbg_csr_mat(csr_mat_3);
-    dbg_csr_mat(csr_mat_4);
-    dbg_csr_mat(csr_mat_5);
+    dbg_csr_mat(csr_mat);
 
     return 0;
 }
